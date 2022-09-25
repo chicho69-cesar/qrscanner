b8a3a287df45fa7463cc216557b65f0438712efa
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-09-24 20:41:27 -0500

    Map personalized

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -14,23 +14,32 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  Completer<GoogleMapController> _controller = Completer();
+  final Completer<GoogleMapController> _controller = Completer();
 
   @override
   Widget build(BuildContext context) {
+    final ScanModel scan = ModalRoute.of(context)!.settings.arguments as ScanModel;
+    
     final CameraPosition initialPoint = CameraPosition(
-      target: LatLng(37.42796133580664, -122.085749655962),
-      zoom: 14.4746,
+      target: scan.getLatLng(),
+      zoom: 17,
+      tilt: 30
     );
 
-    final ScanModel scan = ModalRoute.of(context)!.settings.arguments as ScanModel;
+    Set<Marker> markers = <Marker>{};
+    markers.add(Marker(
+      markerId: const MarkerId('geo-location'),
+      position: scan.getLatLng()
+    ));
     
     return Scaffold(
       appBar: AppBar(
         title: const Text('Mapa'),
       ),
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        myLocationButtonEnabled: false,
+        mapType: MapType.normal,
+        markers: markers,
         initialCameraPosition: initialPoint,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
